2ad571b87e642f54a85e30fc09b2cb97e4a5e779
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2020-03-27 11:11:12 +0100

    CI: removing test files from check
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- ci-scripts/checkCodingFormattingRules.sh
+++ ci-scripts/checkCodingFormattingRules.sh
@@ -74,7 +74,7 @@ then
     for EXTENSION in ${EXTENSION_LIST[@]}
     do
         echo "Checking for all files with .${EXTENSION} extension"
-        FILE_LIST=`tree -n --noreport -i -f -P *.${EXTENSION} | sed -e 's#^\./##' | grep "\.${EXTENSION}"`
+        FILE_LIST=`tree -n --noreport -i -f -P *.${EXTENSION} | sed -e 's#^\./##' | grep -v test | grep "\.${EXTENSION}"`
         for FILE_TO_CHECK in ${FILE_LIST[@]}
         do
             TO_FORMAT=`clang-format -output-replacements-xml ${FILE_TO_CHECK} 2>&1 | grep -v replacements | grep -c replacement`
